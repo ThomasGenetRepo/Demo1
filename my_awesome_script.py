390da5ad52af6893beab17b55b7235a5d22b892f
ThomasGenetRepo <thomas.genet@mosaic.com>
2023-01-07 14:27:43 -0500

    added comments to the code

--- my_awesome_script.py
+++ my_awesome_script.py
@@ -1,13 +1,17 @@
+# This funcion is truely awesome!!! 
 def my_awesome_function(x, y):
     return x+y
 
+# But this function is the awesomest funciton ever!!! 
 def my_even_more_awesome_function(x ,y):
     return x**y
 
+# Let's do some awesome things!!!
 def main():
     x, y = 2, 2
     z1 = my_awesome_function(x, y)
     z2 = my_even_more_awesome_function(x, y)
 
+# lol if main then call main
 if __name__ == '__main__':
     main()
